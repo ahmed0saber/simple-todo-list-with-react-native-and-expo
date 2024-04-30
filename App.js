cba4d8bf961594bc8c3c5c6ccebfc9b4bf95f3d3
Ahmed Saber <ahmed0saber33@gmail.com>
2024-04-30 17:19:05 +0300

    Add first release

---
+++ App.js
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import { Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import DeleteIcon from "./components/delete-icon.jsx"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const saveTodoItems = async (items) => {
+  await AsyncStorage.setItem("stored-todo-items", JSON.stringify(items))
+}
+
+export default function App() {
+  const [todoItems, setTodoItems] = useState([])
+  const [todoTitle, setTodoTitle] = useState("")
+
+  const getTodoItems = async () => {
+    const stored = await AsyncStorage.getItem("stored-todo-items")
+    if (stored) {
+      setTodoItems(JSON.parse(stored))
+      return
+    }
+
+    setTodoItems([])
+  }
+
+  useEffect(() => {
+    getTodoItems()
+  }, [])
+
+  const addNewTask = () => {
+    setTodoItems(prev => [
+      ...prev,
+      {
+        id: Date.now(),
+        title: todoTitle,
+      }
+    ])
+
+    setTodoTitle("")
+  }
+
+  const deleteItem = (id) => {
+    setTodoItems(prev => prev.filter(item => item.id !== id))
+  }
+
+  useEffect(() => {
+    saveTodoItems(todoItems)
+  }, [todoItems])
+
+  return (
+    <SafeAreaView style={styles.safeContainer}>
+      <View>
+        <TextInput
+          style={styles.addInput}
+          placeholder="Enter your task here"
+          value={todoTitle}
+          onChangeText={setTodoTitle}
+        />
+        <Pressable
+          style={styles.addButton}
+          onPress={addNewTask}
+        >
+          <Text style={styles.addButtonText}>Add New Task</Text>
+        </Pressable>
+        <View style={styles.tasksContainer}>
+          {todoItems.length === 0 ? (
+            <View style={styles.taskWrapper}>
+              <Text style={{ flex: 1 }}>No tasks here, try to add some!</Text>
+            </View>
+          ) : null}
+          {todoItems.map(item => (
+            <View key={item.id} style={styles.taskWrapper}>
+              <Text style={{ flex: 1 }}>{item.title}</Text>
+              <Pressable
+                onPress={() => deleteItem(item.id)}
+              >
+                <DeleteIcon
+                  width="24px"
+                  height="24px"
+                  stroke="#000009"
+                />
+              </Pressable>
+            </View>
+          ))}
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+    backgroundColor: "#eff2fb",
+    paddingTop: 48,
+    paddingRight: 24,
+    paddingBottom: 48,
+    paddingLeft: 24,
+  },
+  addInput: {
+    paddingTop: 8,
+    paddingRight: 12,
+    paddingBottom: 8,
+    paddingLeft: 12,
+    fontSize: 16,
+    backgroundColor: "#fbfbfb",
+    borderColor: "#28282828",
+    borderWidth: 1,
+    borderStyle: "solid",
+    marginBottom: 12,
+  },
+  addButton: {
+    padding: 12,
+    backgroundColor: "#000009",
+    fontSize: 16,
+  },
+  addButtonText: {
+    color: "#DEFFF2",
+    textAlign: "center",
+  },
+  tasksContainer: {
+    flexDirection: "column-reverse",
+    marginTop: 24,
+    gap: 12,
+  },
+  taskWrapper: {
+    padding: 12,
+    color: "#000009",
+    borderColor: "#28282828",
+    borderWidth: 1,
+    borderStyle: "solid",
+    fontSize: 16,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    gap: 12,
+  },
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Todo List with React Native and Expo
+
+https://expo.dev/accounts/ahmed0saber/projects/todo-list/builds/9d49fe14-3c42-4f7d-a83f-13a66089549d
+
+## Resources
+
+https://reactnative.dev/docs/environment-setup?guide=quickstart
+
+https://docs.expo.dev/build/setup/#a-react-native-android-andor-ios-project
+
+https://docs.expo.dev/build-reference/apk/
+
